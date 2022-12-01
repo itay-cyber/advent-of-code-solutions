9d9140b73f9c8326cd5964b774578259774c39e2
Itay G <57634628+itay-cyber@users.noreply.github.com>
2022-12-01 22:52:47 +0200

    Add files via upload

---
+++ Day 1/main.py
@@ -0,0 +1,31 @@
+# read input file line by line
+# add line to list "temp numbers"
+# if line is empty, new elf
+# if not, continue reading
+
+
+def sum_arr(array):
+    sum = 0
+    for i in array:
+        sum += int(i)
+    return sum
+
+temp_numbers = []
+sums = []
+lineread = 0
+with open("input.txt", "r") as f:
+    for line in f:
+        newline = line.strip()
+        
+        if newline == "":
+            sums.append(sum_arr(temp_numbers))
+            temp_numbers = []
+        else:
+            temp_numbers.append(int(newline))
+
+sums.sort()
+
+
+print(sums)
+
+
